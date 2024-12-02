200ee7253912b7f74e8a67d16b8b461927256898
Patrick Lehner <patrick@lehnerpat.com>
2024-12-02 18:07:37 +0900

    Add MWE

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { logger } from "@bogeychan/elysia-logger";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(logger())
+  .onRequest(({ request }) => {
+    console.log("request url:", request.url);
+  })
+  .get(
+    "/",
+    ({ query }) => {
+      return "ok";
+    },
+    {
+      query: t.Object({
+        key_1: t.Optional(t.Union([t.Array(t.String()), t.String()])),
+      }),
+    }
+  )
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
